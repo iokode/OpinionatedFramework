c25ff6d5f87c3b44807c23f21a31f74973fce772
Ivan Montilla <imontilla@vadavo.com>
2023-01-15 10:47:48 +0100

    JobExtensions uses Job facade.

--- src/IOKode.OpinionatedFramework.Contracts/Extensions/JobExtensions.cs
+++ src/IOKode.OpinionatedFramework.Contracts/Extensions/JobExtensions.cs
@@ -2,7 +2,6 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Cronos;
-using IOKode.OpinionatedFramework.Foundation;
 using IOKode.OpinionatedFramework.Foundation.Jobs;
 
 namespace IOKode.OpinionatedFramework.Contracts.Extensions;
@@ -12,16 +11,16 @@ public static class JobExtensions
     public static async Task ScheduleAsync(this IJob job, CronExpression interval,
         CancellationToken cancellationToken = default)
     {
-        await Job.ScheduleAsync(job, interval, cancellationToken);
+        await Facades.Job.ScheduleAsync(job, interval, cancellationToken);
     }
 
     public static async Task EnqueueAsync(this IJob job, string queue, CancellationToken cancellationToken)
     {
-        await Job.EnqueueAsync(queue, job, cancellationToken);
+        await Facades.Job.EnqueueAsync(queue, job, cancellationToken);
     }
 
     public static async Task EnqueueWithDelayAsync(this IJob job, string queue, TimeSpan delay, CancellationToken cancellationToken)
     {
-        await Job.EnqueueWithDelayAsync(queue, job, delay, cancellationToken);
+        await Facades.Job.EnqueueWithDelayAsync(queue, job, delay, cancellationToken);
     }
 }
