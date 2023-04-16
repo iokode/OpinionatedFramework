06b1323e7ab6a8516627d1dc544e6d64080211ff
Ivan Montilla <montyclt@outlook.com>
2023-04-16 22:08:33 +0200

    Fixed indentation

--- src/IOKode.OpinionatedFramework.Foundation/Emailing/EmailAddress.cs
+++ src/IOKode.OpinionatedFramework.Foundation/Emailing/EmailAddress.cs
@@ -5,57 +5,57 @@ using IOKode.OpinionatedFramework.Ensuring;
 namespace IOKode.OpinionatedFramework.Foundation.Emailing;
 
 public record EmailAddress
+{
+    public required string Username { get; init; }
+    public required string Host { get; init; }
+
+    /// <exception cref="ArgumentNullException">Email address is null, whitespace or empty.</exception>
+    /// <exception cref="FormatException">Email address is not valid.</exception>
+    public static EmailAddress Parse(string value)
     {
-        public required string Username { get; init; }
-        public required string Host { get; init; }
+        Ensure.Argument(nameof(value)).String.NotWhiteSpace(value);
 
-        /// <exception cref="ArgumentNullException">Email address is null, whitespace or empty.</exception>
-        /// <exception cref="FormatException">Email address is not valid.</exception>
-        public static EmailAddress Parse(string value)
+        if (!new EmailAddressAttribute().IsValid(value))
         {
-            Ensure.Argument(nameof(value)).String.NotWhiteSpace(value);
-
-            if (!new EmailAddressAttribute().IsValid(value))
-            {
-                throw new FormatException("Email address is not valid.");
-            }
-
-            string[] parts = value.Split('@');
-            var username = parts[0];
-            var host = parts[1].ToLowerInvariant();
-
-            return new EmailAddress
-            {
-                Username = username,
-                Host = host
-            };
+            throw new FormatException("Email address is not valid.");
         }
 
-        public static bool TryParse(string value, out EmailAddress? emailAddress)
-        {
-            try
-            {
-                emailAddress = Parse(value);
-                return true;
-            }
-            catch (FormatException)
-            {
-            }
-            catch (ArgumentNullException)
-            {
-            }
+        string[] parts = value.Split('@');
+        var username = parts[0];
+        var host = parts[1].ToLowerInvariant();
 
-            emailAddress = null;
-            return false;
-        }
-
-        public static implicit operator EmailAddress(string s)
+        return new EmailAddress
         {
-            return Parse(s);
-        }
-
-        public override string ToString()
-        {
-            return $"{Username}@{Host}";
-        }
+            Username = username,
+            Host = host
+        };
     }
+
+    public static bool TryParse(string value, out EmailAddress? emailAddress)
+    {
+        try
+        {
+            emailAddress = Parse(value);
+            return true;
+        }
+        catch (FormatException)
+        {
+        }
+        catch (ArgumentNullException)
+        {
+        }
+
+        emailAddress = null;
+        return false;
+    }
+
+    public static implicit operator EmailAddress(string s)
+    {
+        return Parse(s);
+    }
+
+    public override string ToString()
+    {
+        return $"{Username}@{Host}";
+    }
+}
