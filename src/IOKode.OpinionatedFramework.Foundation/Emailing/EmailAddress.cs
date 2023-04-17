ce130f6ae84a0a4597ae71343df880b7d7628378
Ivan Montilla <montyclt@outlook.com>
2023-04-18 01:48:02 +0200

    EmailAddress now accepts display name

--- src/IOKode.OpinionatedFramework.Foundation/Emailing/EmailAddress.cs
+++ src/IOKode.OpinionatedFramework.Foundation/Emailing/EmailAddress.cs
@@ -5,27 +5,79 @@ namespace IOKode.OpinionatedFramework.Foundation.Emailing;
 
 public record EmailAddress
 {
+    // Private constructor enforce using Parse and TryParse for object creation.
+    private EmailAddress()
+    {
+    }
+
+    /// <summary>
+    /// Gets the display name part of the email address.
+    /// </summary>
+    public string? DisplayName { get; init; }
+
+    /// <summary>
+    /// Gets the username part of the email address.
+    /// </summary>
     public required string Username { get; init; }
+
+    /// <summary>
+    /// Gets the host part of the email address.
+    /// </summary>
     public required string Host { get; init; }
 
-    /// <exception cref="ArgumentNullException">Email address is null, whitespace or empty.</exception>
+    /// <summary>
+    /// Parses an email address string and creates an EmailAddress object.
+    /// </summary>
+    /// <param name="value">The email address string to parse, with an optional display name followed by the email address enclosed in angle brackets.</param>
+    /// <exception cref="ArgumentNullException">Email address is null, whitespace, or empty.</exception>
     /// <exception cref="FormatException">Email address is not valid.</exception>
+    /// <returns>An EmailAddress object with the parsed email address and optional display name.</returns>
     public static EmailAddress Parse(string value)
     {
-        Ensure.Argument(nameof(value)).String.NotWhiteSpace(value);
-        Ensure.Exception(new FormatException("Email address is not valid.")).String.Email(value);
+        Ensure.Exception(new ArgumentNullException(nameof(value))).String.NotWhiteSpace(value);
 
-        string[] parts = value.Split('@');
+        string? displayName = null;
+        string emailValue;
+
+        if (value.Contains('<') && value.Contains('>'))
+        {
+            int startBracket = value.IndexOf('<');
+            int endBracket = value.IndexOf('>');
+            displayName = value.Substring(0, startBracket).Trim();
+
+            if (string.IsNullOrWhiteSpace(displayName))
+            {
+                displayName = null;
+            }
+            
+            emailValue = value.Substring(startBracket + 1, endBracket - startBracket - 1);
+        }
+        else
+        {
+            emailValue = value;
+        }
+
+        Ensure.Exception(new FormatException("Email address is not valid.")).String.Email(emailValue);
+
+        string[] parts = emailValue.Split('@');
         var username = parts[0];
         var host = parts[1].ToLowerInvariant();
 
         return new EmailAddress
         {
             Username = username,
-            Host = host
+            Host = host,
+            DisplayName = displayName
         };
     }
 
+    /// <summary>
+    /// Tries to parse the provided string value into an <see cref="EmailAddress"/> instance.
+    /// </summary>
+    /// <param name="value">The email address string to parse, with an optional display name followed by the email address enclosed in angle brackets.</param>
+    /// <param name="emailAddress">When this method returns, contains the <see cref="EmailAddress"/> instance,
+    /// if the conversion succeeded, or null if the conversion failed.</param>
+    /// <returns>true if the value was converted successfully; otherwise, false.</returns>
     public static bool TryParse(string value, out EmailAddress? emailAddress)
     {
         try
@@ -44,13 +96,22 @@ public record EmailAddress
         return false;
     }
 
+    /// <summary>
+    /// Defines an implicit conversion of a string to an <see cref="EmailAddress"/> instance.
+    /// </summary>
+    /// <param name="s">The email address string to parse, with an optional display name followed by the email address enclosed in angle brackets.</param>
+    /// <returns>An <see cref="EmailAddress"/> instance.</returns>
     public static implicit operator EmailAddress(string s)
     {
         return Parse(s);
     }
 
+    /// <summary>
+    /// Returns a string representation of the email address.
+    /// </summary>
+    /// <returns>A string that represents the current email address.</returns>
     public override string ToString()
     {
-        return $"{Username}@{Host}";
+        return DisplayName == null ? $"{Username}@{Host}" : $"{DisplayName} <{Username}@{Host}>";
     }
 }
---
+++ src/IOKode.OpinionatedFramework.Tests/Foundation/Emailing/EmailAddressTests.cs
@@ -0,0 +1,101 @@
+using System;
+using IOKode.OpinionatedFramework.Foundation.Emailing;
+
+namespace IOKode.OpinionatedFramework.Tests.Foundation.Emailing;
+
+public class EmailAddressTests
+{
+    [Fact]
+    public void Parse_ValidEmailAddressWithoutDisplayName_ReturnsEmailAddress()
+    {
+        // Arrange
+        string input = "johndoe@example.com";
+
+        // Act
+        EmailAddress emailAddress = EmailAddress.Parse(input);
+
+        // Assert
+        Assert.Equal("johndoe", emailAddress.Username);
+        Assert.Equal("example.com", emailAddress.Host);
+        Assert.Null(emailAddress.DisplayName);
+    }
+
+    [Fact]
+    public void Parse_ValidEmailAddressWithDisplayName_ReturnsEmailAddress()
+    {
+        // Arrange
+        string input = "John Doe <johndoe@example.com>";
+
+        // Act
+        EmailAddress emailAddress = EmailAddress.Parse(input);
+
+        // Assert
+        Assert.Equal("johndoe", emailAddress.Username);
+        Assert.Equal("example.com", emailAddress.Host);
+        Assert.Equal("John Doe", emailAddress.DisplayName);
+    }
+
+    [Theory]
+    [InlineData("johndoe")]
+    [InlineData("johndoeexample@")]
+    public void Parse_InvalidEmailAddress_ThrowsFormatException(string input)
+    {
+        Assert.Throws<FormatException>(() => EmailAddress.Parse(input));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Parse_EmptyEmailAddress_ThrowsArgumentNullException(string input)
+    {
+        Assert.Throws<ArgumentNullException>(() => EmailAddress.Parse(input));
+    }
+
+    [Fact]
+    public void TryParse_ValidEmailAddress_ReturnsTrueAndEmailAddress()
+    {
+        // Arrange
+        string input = "johndoe@example.com";
+        EmailAddress? emailAddress;
+
+        // Act
+        bool result = EmailAddress.TryParse(input, out emailAddress);
+
+        // Assert
+        Assert.True(result);
+        Assert.NotNull(emailAddress);
+        Assert.Equal("johndoe", emailAddress.Username);
+        Assert.Equal("example.com", emailAddress.Host);
+        Assert.Null(emailAddress.DisplayName);
+    }
+
+    [Fact]
+    public void TryParse_ValidEmailAddressWithDisplayName_ReturnsTrueAndEmailAddress()
+    {
+        // Arrange
+        string input = "John Doe <johndoe@example.com>";
+        EmailAddress? emailAddress;
+
+        // Act
+        bool result = EmailAddress.TryParse(input, out emailAddress);
+
+        // Assert
+        Assert.True(result);
+        Assert.NotNull(emailAddress);
+        Assert.Equal("johndoe", emailAddress.Username);
+        Assert.Equal("example.com", emailAddress.Host);
+        Assert.Equal("John Doe", emailAddress.DisplayName);
+    }
+
+    [Fact]
+    public void Parse_ValidEmailInAngleBracketsWithoutDisplayName_ReturnsEmailAddress()
+    {
+        string input = "<johndoe@example.com>";
+        var emailAddress = EmailAddress.Parse(input);
+
+        Assert.Equal("johndoe", emailAddress.Username);
+        Assert.Equal("example.com", emailAddress.Host);
+        Assert.Null(emailAddress.DisplayName);
+    }
+}
