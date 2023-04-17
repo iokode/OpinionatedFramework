775d42deaf4dad0f487bc0a6920236f5493042a4
Ivan Montilla <montyclt@outlook.com>
2023-04-18 01:48:41 +0200

    Added doc-comments to EmailAttachment record

--- src/IOKode.OpinionatedFramework.Foundation/Emailing/EmailAttachment.cs
+++ src/IOKode.OpinionatedFramework.Foundation/Emailing/EmailAttachment.cs
@@ -3,9 +3,23 @@ using System.Net.Mime;
 
 namespace IOKode.OpinionatedFramework.Foundation.Emailing;
 
+/// <summary>
+/// Represents an email attachment to be sent with an email message.
+/// </summary>
 public record EmailAttachment
 {
+    /// <summary>
+    /// Gets the file name of the attachment, including its file extension.
+    /// </summary>
     public required string FileName { get; init; }
+
+    /// <summary>
+    /// Gets the content stream of the attachment.
+    /// </summary>
     public required Stream Content { get; init; }
+
+    /// <summary>
+    /// Gets or sets the content type (MIME type) of the attachment.
+    /// </summary>
     public ContentType? ContentType { get; init; }
 }
