45e4155c618f6ecba0812618c8e8665dfd13b4d8
Ivan Montilla <montyclt@outlook.com>
2023-05-16 20:26:55 +0200

    Renamed tests

--- src/IOKode.OpinionatedFramework.Foundation.Tests/Ensuring/Ensurers/EnumerableEnsurerTests.cs
+++ src/IOKode.OpinionatedFramework.Foundation.Tests/Ensuring/Ensurers/EnumerableEnsurerTests.cs
@@ -11,133 +11,133 @@ public class EnumerableEnsurerTests
     private readonly IEnumerable<object> _emptyCollection = new List<object>();
 
     [Fact]
-    public void TestNotEmpty_WithNotEmptyCollection()
+    public void NotEmpty_WithNotEmptyCollection()
     {
         bool result = EnumerableEnsurer.NotEmpty(_collectionWithThreeItems);
         Assert.True(result);
     }
 
     [Fact]
-    public void TestNotEmpty_WithEmptyCollection()
+    public void NotEmpty_WithEmptyCollection()
     {
         bool emptyResult = EnumerableEnsurer.NotEmpty(_emptyCollection);
         Assert.False(emptyResult);
     }
 
     [Fact]
-    public void TestEmpty_WithEmptyCollection()
+    public void Empty_WithEmptyCollection()
     {
         bool result = EnumerableEnsurer.Empty(_emptyCollection);
         Assert.True(result);
     }
 
     [Fact]
-    public void TestEmpty_WithNotEmptyCollection()
+    public void Empty_WithNotEmptyCollection()
     {
         bool notEmptyResult = EnumerableEnsurer.Empty(_collectionWithOneItem);
         Assert.False(notEmptyResult);
     }
 
     [Fact]
-    public void TestCount_WithCountEqualToExpected()
+    public void Count_WithCountEqualToExpected()
     {
         bool result = EnumerableEnsurer.Count(_collectionWithThreeItems, 3);
         Assert.True(result);
     }
 
     [Fact]
-    public void TestCount_WithCountNotEqualToExpected()
+    public void Count_WithCountNotEqualToExpected()
     {
         bool countIsZeroResult = EnumerableEnsurer.Count(_collectionWithThreeItems, 1);
         Assert.False(countIsZeroResult);
     }
 
     [Fact]
-    public void TestMinCount_WithMinCountEqualToExpected()
+    public void MinCount_WithMinCountEqualToExpected()
     {
         bool result = EnumerableEnsurer.AtLeastNItems(_collectionWithTwoItems, 2);
         Assert.True(result);
     }
 
     [Fact]
-    public void TestMinCount_WithMinCountNotEqualToExpected()
+    public void MinCount_WithMinCountNotEqualToExpected()
     {
         bool minCountIsZeroResult = EnumerableEnsurer.AtLeastNItems(_emptyCollection, 1);
         Assert.False(minCountIsZeroResult);
     }
 
     [Fact]
-    public void TestMaxCount_WithMaxCountEqualToExpected()
+    public void MaxCount_WithMaxCountEqualToExpected()
     {
         bool result = EnumerableEnsurer.UpToNItems(_collectionWithThreeItems, 4);
         Assert.True(result);
     }
 
     [Fact]
-    public void TestMaxCount_WithMaxCountNotEqualToExpected()
+    public void MaxCount_WithMaxCountNotEqualToExpected()
     {
         bool maxCountIsZeroResult = EnumerableEnsurer.UpToNItems(_emptyCollection, 1);
         Assert.True(maxCountIsZeroResult);
     }
 
     [Fact]
-    public void TestCountBetween_WithCountInRange()
+    public void CountBetween_WithCountInRange()
     {
         bool result = EnumerableEnsurer.CountBetween(_collectionWithTwoItems, 1, 3);
         Assert.True(result);
     }
 
     [Fact]
-    public void TestCountBetween_WithCountBelowRange()
+    public void CountBetween_WithCountBelowRange()
     {
         bool countBelowRangeResult = EnumerableEnsurer.CountBetween(_collectionWithOneItem, 2, 4);
         Assert.False(countBelowRangeResult);
     }
 
     [Fact]
-    public void TestCountBetween_WithCountAboveRange()
+    public void CountBetween_WithCountAboveRange()
     {
         bool countAboveRangeResult = EnumerableEnsurer.CountBetween(_collectionWithThreeItems, 1, 2);
         Assert.False(countAboveRangeResult);
     }
 
     [Fact]
-    public void TestIn_WithAllValuesPresent()
+    public void In_WithAllValuesPresent()
     {
         bool result = EnumerableEnsurer.In(_collectionWithThreeItems, 1, 2, 3);
         Assert.True(result);
     }
 
     [Fact]
-    public void TestIn_WithSomeValuesMissing()
+    public void In_WithSomeValuesMissing()
     {
         bool result = EnumerableEnsurer.In(_collectionWithTwoItems, 1, 2, 3);
         Assert.False(result);
     }
 
     [Fact]
-    public void TestAll_WithAllItemsSatisfyingPredicate()
+    public void All_WithAllItemsSatisfyingPredicate()
     {
         bool result = EnumerableEnsurer.All(_collectionWithThreeItems, x => x is int);
         Assert.True(result);
     }
 
     [Fact]
-    public void TestAll_WithSomeItemsNotSatisfyingPredicate()
+    public void All_WithSomeItemsNotSatisfyingPredicate()
     {
         bool result = EnumerableEnsurer.All(_collectionWithThreeItems, x => x is string);
         Assert.False(result);
     }
 
     [Fact]
-    public void TestAny_WithAtLeastOneItemSatisfyingPredicate()
+    public void Any_WithAtLeastOneItemSatisfyingPredicate()
     {
         bool result = EnumerableEnsurer.Any(_collectionWithThreeItems, x => x is int);
         Assert.True(result);
     }
 
     [Fact]
-    public void TestAny_WithNoItemsSatisfyingPredicate()
+    public void Any_WithNoItemsSatisfyingPredicate()
     {
         bool result = EnumerableEnsurer.Any(_collectionWithThreeItems, x => x is string);
         Assert.False(result);
