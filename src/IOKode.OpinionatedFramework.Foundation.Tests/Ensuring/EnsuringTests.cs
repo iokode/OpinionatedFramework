861d81bb20d8faa6786d3b93c18f7fa1fd7337be
Ivan Montilla <montyclt@outlook.com>
2023-05-25 03:09:29 +0200

    Added ensuring unit tests

---
+++ src/IOKode.OpinionatedFramework.Foundation.Tests/Ensuring/EnsuringTests.cs
@@ -0,0 +1,39 @@
+using System;
+using IOKode.OpinionatedFramework.Ensuring;
+
+namespace IOKode.OpinionatedFramework.Foundation.Tests.Ensuring;
+
+public class EnsuringTests
+{
+    [Fact]
+    public void DoesNotThrowsWhenValidationPasses()
+    {
+        Ensure.Boolean.IsTrue(true).ElseThrows(new _CustomException());
+    }
+    
+    [Fact]
+    public void ThrowsWhenValidationNotPasses()
+    {
+        Assert.Throws<_CustomException>(() =>
+        {
+            Ensure.Boolean.IsTrue(false).ElseThrows(new _CustomException());
+        });
+    }
+
+    [Fact]
+    public void ThrowsArgumentNull()
+    {
+        var exception = Assert.Throws<ArgumentNullException>(() =>
+        {
+            string? obj = null;
+            Ensure.ArgumentNotNull(obj);
+        });
+        
+        Assert.Equal("obj", exception.ParamName);
+    }
+
+    private class _CustomException : Exception
+    {
+        
+    }
+}
