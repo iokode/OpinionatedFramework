dea56f27b2e123eade4a98b661d4ccd124001247
Ivan Montilla <montyclt@outlook.com>
2024-05-26 23:59:23 +0200

    Improve log feature

---
+++ src/Foundation/Logging/ILogging.cs
@@ -0,0 +1,318 @@
+using System;
+using System.Diagnostics;
+using IOKode.OpinionatedFramework.Facades;
+using Microsoft.Extensions.Logging;
+
+namespace IOKode.OpinionatedFramework.Logging;
+
+[AddToFacade("Log")]
+public interface ILogging
+{
+    public ILogger FromCategory(string category);
+
+    public ILogger FromCategory(Type categoryType);
+
+    public ILogger FromCategory<TCategory>()
+    {
+        var categoryType = typeof(TCategory);
+        var logger = FromCategory(categoryType);
+
+        return logger;
+    }
+
+    private ILogger FromCaller()
+    {
+        var callerType = new StackFrame(1).GetMethod()!.DeclaringType!;
+        var logger = FromCategory(callerType);
+
+        return logger;
+    }
+
+    #region Debug
+
+    /// <summary>
+    /// Formats and writes a debug log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Debug(EventId eventId, Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Debug, eventId, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a debug log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Debug(EventId eventId, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Debug, eventId, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a debug log message.
+    /// </summary>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Debug(Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Debug, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a debug log message.
+    /// </summary>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Debug(string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Debug, message, args);
+    }
+
+    #endregion
+
+    #region Trace
+
+    /// <summary>
+    /// Formats and writes a trace log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Trace(EventId eventId, Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Trace, eventId, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a trace log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Trace(EventId eventId, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Trace, eventId, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a trace log message.
+    /// </summary>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Trace(Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Trace, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a trace log message.
+    /// </summary>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Trace(string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Trace, message, args);
+    }
+
+    #endregion
+
+    #region Info
+
+    /// <summary>
+    /// Formats and writes an informational log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Info(EventId eventId, Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Information, eventId, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes an informational log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Info(EventId eventId, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Information, eventId, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes an informational log message.
+    /// </summary>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Info(Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Information, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes an informational log message.
+    /// </summary>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Info(string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Information, message, args);
+    }
+
+    #endregion
+
+    #region Warning
+
+    /// <summary>
+    /// Formats and writes a warning log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Warn(EventId eventId, Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Warning, eventId, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a warning log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Warn(EventId eventId, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Warning, eventId, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a warning log message.
+    /// </summary>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Warn(Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Warning, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a warning log message.
+    /// </summary>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Warn(string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Warning, message, args);
+    }
+
+    #endregion
+
+    #region Error
+
+    /// <summary>
+    /// Formats and writes an error log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Error(EventId eventId, Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Error, eventId, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes an error log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Error(EventId eventId, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Error, eventId, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes an error log message.
+    /// </summary>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Error(Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Error, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes an error log message.
+    /// </summary>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Error(string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Error, message, args);
+    }
+
+    #endregion
+
+    #region Critical
+
+    /// <summary>
+    /// Formats and writes a critical log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Critical(EventId eventId, Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Critical, eventId, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a critical log message.
+    /// </summary>
+    /// <param name="eventId">The event id associated with the log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Critical(EventId eventId, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Critical, eventId, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a critical log message.
+    /// </summary>
+    /// <param name="exception">The exception to log.</param>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Critical(Exception? exception, string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Critical, exception, message, args);
+    }
+
+    /// <summary>
+    /// Formats and writes a critical log message.
+    /// </summary>
+    /// <param name="message">Format string of the log message in message template format. Example: <c>"User {User} logged in from {Address}"</c></param>
+    /// <param name="args">An object array that contains zero or more objects to format.</param>
+    public void Critical(string? message, params object?[] args)
+    {
+        FromCaller().Log(LogLevel.Critical, message, args);
+    }
+
+    #endregion
+}
