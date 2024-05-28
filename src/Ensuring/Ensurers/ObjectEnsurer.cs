410cc31e26fb9ab1fe2c743fd1f1606140eb7820
Ivan Montilla <montyclt@outlook.com>
2024-05-28 19:47:41 +0200

    Fix ObjectEnsurer placement

--- src/Ensuring/Ensurers/ObjectEnsurer.cs
+++ src/Ensuring/Ensurers/ObjectEnsurer.cs
@@ -1,8 +1,9 @@
+using System;
+
 namespace IOKode.OpinionatedFramework.Ensuring.Ensurers;
 
 /// <summary>
 /// Provides methods for validating conditions related to objects within the Ensure API.
-/// Decorated with the EnsurerAttribute, it is a part of the Ensure validation mechanism.
 /// </summary>
 /// <remarks>
 /// As part of the Ensure API mechanism, this class is static.
@@ -11,22 +12,80 @@ namespace IOKode.OpinionatedFramework.Ensuring.Ensurers;
 public static class ObjectEnsurer
 {
     /// <summary>
-    /// Validates that the given object is not null.
+    /// Determines whether the specified object is null.
     /// </summary>
-    /// <param name="value">The object to be validated.</param>
-    /// <returns>A boolean value indicating whether the object is not null. Returns true if the object is not null; otherwise, false.</returns>
+    /// <remarks>
+    /// This method uses the == operator to compare the object with null, that means that the comparision couldn't
+    /// be strictly with null if the operator is overriden in the class of the object.
+    /// </remarks>
+    /// <param name="value">The object to check for null.</param>
+    /// <returns><c>true</c> if the specified object is null; otherwise, <c>false</c>.</returns>
+    public static bool IsNull(object? value)
+    {
+        return value == null;
+    }
+
+    /// <summary>
+    /// Determines whether the specified object is not null.
+    /// </summary>
+    /// /// <remarks>
+    /// This method uses the != operator to compare the object with null, that means that the comparision couldn't
+    /// be strictly with null if the operator is overriden in the class of the object.
+    /// </remarks>
+    /// <param name="value">The object to check for null.</param>
+    /// <returns><c>true</c> if the specified object is not null; otherwise, <c>false</c>.</returns>
     public static bool NotNull(object? value)
     {
         return value != null;
     }
 
     /// <summary>
-    /// Validates that the given object is null.
+    /// Determines whether the specified object instances are considered equal by comparing their values
+    /// using the <see cref="object.Equals(object)"/> method from <paramref name="value"/>.
     /// </summary>
-    /// <param name="value">The object to be validated.</param>
-    /// <returns>A boolean value indicating whether the object is null. Returns true if the object is null; otherwise, false.</returns>
-    public static bool IsNull(object? value)
+    /// <param name="value">The first object to compare. Must not be null.</param>
+    /// <param name="other">The second object to compare.</param>
+    /// <returns><c>true</c> if the specified objects are considered equal; otherwise, <c>false</c>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static bool Equivalent(object value, object other)
     {
-        return value == null;
+        Ensure.ArgumentNotNull(value);
+        return value.Equals(other);
+    }
+
+    /// <summary>
+    /// Determines whether the specified object instances are not considered equal by comparing their values
+    /// using the <see cref="object.Equals(object)"/> method from <paramref name="value"/>.
+    /// </summary>
+    /// <param name="value">The first object to compare. Must not be null.</param>
+    /// <param name="other">The second object to compare.</param>
+    /// <returns><c>true</c> if the specified objects are not considered equal; otherwise, <c>false</c>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static bool NotEquivalent(object value, object other)
+    {
+        Ensure.ArgumentNotNull(value);
+        return !value.Equals(other);
+    }
+
+    /// <summary>
+    /// Determines whether the specified objects are the same instance.
+    /// </summary>
+    /// <param name="value">The first object to compare.</param>
+    /// <param name="other">The second object to compare.</param>
+    /// <returns><c>true</c> if the specified objects are the same instance; otherwise, <c>false</c>.</returns>
+    public static bool Same(object? value, object? other)
+    {
+        return ReferenceEquals(value, other);
+    }
+
+    /// <summary>
+    /// Determines whether the specified objects are not the same instance.
+    /// </summary>
+    /// <param name="value">The first object to compare.</param>
+    /// <param name="other">The second object to compare.</param>
+    /// <returns><c>true</c> if the specified objects are not the same instance; otherwise, <c>false</c>.</returns>
+    public static bool NotSame(object? value, object? other)
+    {
+        return !ReferenceEquals(value, other);
     }
 }
