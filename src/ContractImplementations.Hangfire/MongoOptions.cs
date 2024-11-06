0692b7e461cb7277f7390d4eff4c749fe763659d
Angel <ruiz.angeling@outlook.com>
2024-11-06 12:00:27 -0600

    Delete projects related to hangfire

--- src/ContractImplementations.Hangfire/MongoOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace IOKode.OpinionatedFramework.ContractImplementations.Hangfire;
-
-public class MongoOptions
-{
-    public string Database { get; set; } = string.Empty;
-    public string ConnectionString { get; set; } = string.Empty;
-}
