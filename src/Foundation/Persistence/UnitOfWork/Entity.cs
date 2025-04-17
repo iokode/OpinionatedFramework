db5dbc255ec88be37827e9b546cdab4362ebd01e
Ivan Montilla <ivan@iokode.net>
2025-04-17 23:35:59 +0200

    refactor(persistence): remove generic Entity<TId> class definition

--- src/Foundation/Persistence/UnitOfWork/Entity.cs
+++ src/Foundation/Persistence/UnitOfWork/Entity.cs
@@ -1,8 +1,3 @@
 namespace IOKode.OpinionatedFramework.Persistence.UnitOfWork;
 
 public abstract class Entity;
-
-public abstract class Entity<TId> : Entity
-{
-    public TId? Id { get; protected set; }
-}
