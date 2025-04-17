3a299030f07e4d29616ca96ee9a8624558c72419
Ivan Montilla <ivan@iokode.net>
2025-04-17 23:35:36 +0200

    fix(hangfire): remove unnecessary configuration parameter in AddHangfireJobsImplementations

--- src/ContractImplementations.Hangfire/Jobs/ServiceExtensions.cs
+++ src/ContractImplementations.Hangfire/Jobs/ServiceExtensions.cs
@@ -8,11 +8,9 @@ namespace IOKode.OpinionatedFramework.ContractImplementations.Hangfire.Jobs;
 
 public static class ServiceExtensions
 {
-    public static void AddHangfireJobsImplementations(this IOpinionatedServiceCollection services, Action<IGlobalConfiguration> configuration)
+    public static void AddHangfireJobsImplementations(this IOpinionatedServiceCollection services)
     {
         services.AddSingleton<IJobEnqueuer, HangfireJobEnqueuer>();
         services.AddSingleton<IJobScheduler, HangfireJobScheduler>();
-
-        configuration.Invoke(GlobalConfiguration.Configuration);
     }
 }
