20ea060183cd849dce8d2e16fc486ce2ca52e8d0
Angel <ruiz.angeling@outlook.com>
2026-02-20 15:38:25 -0600

    docs(exceptions): add XML doc for ResourceNotFoundException

--- src/AspNetCoreIntegrations/Exceptions/ResourceNotFoundException.cs
+++ src/AspNetCoreIntegrations/Exceptions/ResourceNotFoundException.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace IOKode.OpinionatedFramework.AspNetCoreIntegrations.Exceptions;
 
+/// <summary>
+/// The exception that is thrown when a resource is not found.
+/// </summary>
 public class ResourceNotFoundException : Exception
 {
     private const string ExceptionMessage = "The requested resource was not found.";
